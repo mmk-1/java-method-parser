bcf35ebf44d3e60157a214b6322ddf9529f15637
Mahdi Khosravi <mahdi.khosravi@metu.edu.tr>
2023-08-11 20:07:08 +0000

    Roaster library

---
+++ src/main/java/com/example/Example.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.util.List;
+
+public class Example {
+
+    private String s;
+
+    public Example() {
+    }
+
+    public Example(List<String> s) {
+        // this.s = s;
+
+    }
+
+    public void a() {
+
+    }
+
+    private int a;
+
+    protected void b() {
+
+    }
+
+    private void c() {
+
+    }
+
+    private int d(String s) {
+        int a = 0;
+        s = "hello";
+
+        return a;
+    }
+}
